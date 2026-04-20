ef5512a54851770e5c6f7e40a1aa71354877a70a
ScriptAndCompile <scriptandcompile@gmail.com>
2026-04-20 11:35:17 -0700

    lib.rs in common shouldn't be exposing dll plugin meta datat

--- crates/platform/win-common/rine-common-ntdll/src/lib.rs
+++ crates/platform/win-common/rine-common-ntdll/src/lib.rs
@@ -1,54 +1,5 @@
+#![allow(unsafe_op_in_unsafe_fn)]
+
 pub mod file;
 pub mod process;
 pub mod rtl;
-
-use rine_dlls::{DllPlugin, Export, StubExport, as_win_api};
-
-pub struct NtdllPlugin32;
-
-impl DllPlugin for NtdllPlugin32 {
-    fn dll_names(&self) -> &[&str] {
-        &["ntdll.dll"]
-    }
-
-    fn exports(&self) -> Vec<Export> {
-        vec![Export::Func(
-            "NtCreateFile",
-            as_win_api!(file::nt_create_file),
-        )]
-    }
-
-    fn stubs(&self) -> Vec<StubExport> {
-        vec![
-            StubExport {
-                name: "NtReadFile",
-                func: as_win_api!(file::nt_read_file),
-            },
-            StubExport {
-                name: "NtWriteFile",
-                func: as_win_api!(file::nt_write_file),
-            },
-            StubExport {
-                name: "NtClose",
-                func: as_win_api!(file::nt_close),
-            },
-            StubExport {
-                name: "NtQueryInformationFile",
-                func: as_win_api!(file::nt_query_information_file),
-            },
-            StubExport {
-                name: "NtTerminateProcess",
-                func: as_win_api!(process::nt_terminate_process),
-            },
-            // rtl.rs
-            StubExport {
-                name: "RtlInitUnicodeString",
-                func: as_win_api!(rtl::rtl_init_unicode_string),
-            },
-            StubExport {
-                name: "RtlGetVersion",
-                func: as_win_api!(rtl::rtl_get_version),
-            },
-        ]
-    }
-}
