883d2a5345359d6363a0976e27d2ea25274295de
ScriptAndCompile <scriptandcompile@gmail.com>
2026-04-21 10:42:36 -0700

    Renamed window_manager.rs to window_livecycle.rs to match 32bit and 64bit implementations

--- crates/platform/win-common/rine-common-user32/src/lib.rs
+++ crates/platform/win-common/rine-common-user32/src/lib.rs
@@ -11,7 +11,7 @@
 pub mod backend;
 pub mod class_registration;
 pub mod message_queue;
-pub mod window_manager;
+pub mod window_lifecycle;
 pub mod window_text;
 
 pub use class_registration::unregister_class;
@@ -19,7 +19,7 @@ pub use message_queue::{
     def_window_proc, dispatch_message, get_message, peek_message, post_message, post_quit_message,
     send_message, translate_message,
 };
-pub use window_manager::{create_window, destroy_window, show_window, update_window};
+pub use window_lifecycle::{create_window, destroy_window, show_window, update_window};
 pub use window_text::{
     get_window_text_a, get_window_text_length, get_window_text_w, set_window_text,
 };
